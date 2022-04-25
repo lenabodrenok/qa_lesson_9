05f2a5f8f212f48104a9488b283d45be2decaaec
Elena <lena.bodrenok@gmail.com>
2022-04-25 23:13:52 +0300

    add answer

--- src/test/java/guru/qa/findJUnit5Test.java
+++ src/test/java/guru/qa/findJUnit5Test.java
@@ -12,8 +12,14 @@ public class findJUnit5Test {
     void selenideGithubCodeJUnit5(){
         open("https://github.com/selenide/selenide");
         $("#wiki-tab").click();
-        $("#wiki-pages-filter").setValue("SoftAssertions");
+        $("#wiki-pages-filter").setValue("softassertions");
         $(By.linkText("SoftAssertions")).shouldBe(visible).click();
         $$(".markdown-body").findBy(visible).shouldHave(text("Using JUnit5 extend test class"));
     }
 }
+
+// Есть ли разница между $("h1 div"); и $("h1").$("div"); - может ли привести к тому что, поиск найдёт разные элементы?
+// Если может - приведите пример, когда.
+// Разница есть:
+// $("h1 div") - ищем одновременно "h1 div" в одном элементе
+// $("h1").$("div") - ищем первый "div" внутри первого "h1"
